4d22b852aef3b185862b3baaf06d1e97a8e39b5c
actionchain <actionchain@chainguard.dev>
2026-05-28 10:06:04 +0000

    Update chrislennon/action-aws-cli@v1

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# chrislennon-action-aws-cli
+# action-aws-cli
+
+Action to install AWS cli
+
+## Usage
+
+Example
+````yaml
+name: Example
+
+on:
+  push
+
+jobs:
+  listS3:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: chrislennon/action-aws-cli@v1
+      - run: aws s3 ls
+        env:
+          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+````
+
+## Privacy
+
+This Action contacts Chainguard's licensing server to verify authorization. Connection metadata (IP address, GitHub repository identifier, timestamp, and any metadata encoded in the auth token) is transmitted to Chainguard, Inc. even if authorization is denied in accordance with our [Privacy Notice](https://www.chainguard.dev/legal/privacy-notice)
---
+++ src/main.ts
@@ -0,0 +1,59 @@
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as os from 'os'
+import { promisify } from 'util'
+import { exec } from 'child_process'
+import * as path from 'path'
+
+const execP = promisify(exec)
+const osPlat: string = os.platform()
+
+export async function run(): Promise<void> {
+  try {
+    let toolPath: string
+    toolPath = tc.find('aws', '*')
+
+    if (!toolPath) {
+      // download
+      const downloadUrl = `https://s3.amazonaws.com/aws-cli/awscli-bundle.zip`
+      const downloadPath = await tc.downloadTool(downloadUrl)
+
+      // extract
+      let extPath: string
+      if (osPlat === 'darwin') {
+        extPath = await tc.extractTar(downloadPath)
+      } else if (osPlat === 'linux') {
+        // extPath = await tc.extractZip(downloadPath)
+        extPath = __dirname
+        await execP(`unzip ${downloadPath} -d ${extPath}`)
+      } else {
+        extPath = await tc.extractZip(downloadPath)
+      }
+
+      // install
+      const installPath = `${extPath}/.local/lib/aws`
+      const binPath = `${installPath}/bin`
+
+      await execP(`${extPath}/awscli-bundle/install -i ${installPath}`)
+
+      await new Promise(async (resolve, reject)=> { // wrapped in promise to control stderr
+        exec(`${binPath}/aws --version`, async (err, stout, sterr) => {
+          const response = err ? stout : sterr
+          const regex = /(\d+\.)(\d+\.)(\d+)/g
+          const cliVersion = response.match(regex)
+          if (cliVersion !== null) {
+            const cachedPath = await tc.cacheDir(binPath, 'aws', cliVersion[0])
+            core.addPath(cachedPath)
+            resolve()
+          } else {
+            reject()
+          }
+        })
+      })
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+run()
